dd44ee0e7177671efde9889b7ad544d4111aad9d
Ian <imatthews009@gmail.com>
2018-01-16 15:54:14 -0800

    added onClick handler to work in Person.js function

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ class App extends Component {
     ]
   }
   // handler means handling event handler
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked');
     // DON'T DO THIS this.state.persons[0].name = 'Iaaan';
     this.setState({
       persons: [
-        {name: 'Iaaaan', age: 24},
-        {name: 'Bob', age: 20},
-        {name: 'Olivia', age: 22},
+        {name: newName, age: 24},
+        {name: newName, age: 20},
+        {name: newName, age: 22},
       ] 
     })
   }
@@ -30,10 +30,20 @@ class App extends Component {
         <h1>Hi, I'm a react App</h1>
         {/* uppercase C in onClick unlike normal JS */}
         {/* if you add () at the end of onclick function it executes the function immediately. So do not add () */}
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[0].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[0].age}/>
+        <button onClick={() => this.switchNameHandler('BOOOOO')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+        />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[0].age}
+          // passing the function that changes the state to the person function in person.js
+          click={this.switchNameHandler.bind(this, 'Ian!!!!')}>My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+            age={this.state.persons[0].age}
+        />
       </div>
       // code above is processed like the code below
       // React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'))
