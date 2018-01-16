7afcdd75741a639fb5a245ac3f2a43f0dc0a76c7
Ian <imatthews009@gmail.com>
2018-01-16 13:17:58 -0800

    added onClick handler to change persons array in the state object. Used setState.

--- src/App.js
+++ src/App.js
@@ -12,11 +12,25 @@ class App extends Component {
       {name: 'Olivia', age: 22},
     ]
   }
+  // handler means handling event handler
+  switchNameHandler = () => {
+    // console.log('Was clicked');
+    // DON'T DO THIS this.state.persons[0].name = 'Iaaan';
+    this.setState({
+      persons: [
+        {name: 'Iaaaan', age: 24},
+        {name: 'Bob', age: 20},
+        {name: 'Olivia', age: 22},
+      ] 
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react App</h1>
-        <button>Switch Name</button>
+        {/* uppercase C in onClick unlike normal JS */}
+        {/* if you add () at the end of onclick function it executes the function immediately. So do not add () */}
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[0].age}>My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[0].age}/>
