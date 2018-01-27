b46d022e22ca3c1503c17f91c0ef3164887930b6
Ian <imatthews009@gmail.com>
2018-01-27 11:59:33 -0800

    fixed classname on Person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import './Person.css'
+import classes from './Person.css'
 // import Radium from 'radium'
 
 // props pull the attributes assigned in the App.js code
 const person = (props) => {
   return (
-    <div className="Person">
+    <div className={classes.Person}>
       <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
       {/* children includes any elements between opening and closing tag of component */}
