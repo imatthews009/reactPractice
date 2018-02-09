9d4d7c3d08f1fbf1e6cd337c28e2bbec44fb6990
Ian <imatthews009@gmail.com>
2018-02-08 17:24:39 -0800

    changed Person and Persons components to be stateful

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { Component }from 'react'
 import classes from './Person.css'
 // import Radium from 'radium'
 
 // props pull the attributes assigned in the App.js code
-const person = (props) => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
-      <p>{props.children}</p>
-      {/* children includes any elements between opening and closing tag of component */}
-      <input type='text' onChange={props.changed} value={props.name } />
-    </div>
-  )
-};
+class Person extends Component {
+  render () {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
+        <p>{this.props.children}</p>
+        {/* children includes any elements between opening and closing tag of component */}
+        <input type='text' onChange={this.props.changed} value={this.props.name } />
+      </div>
+    )
+  }
+}
 
-export default person;
+
+export default Person;
